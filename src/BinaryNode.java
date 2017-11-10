1783613a7938cb90e655578adc22e4540e9dc016
karolyi15 <karolyi15@hotmail.com>
2017-11-09 20:51:17 -0600

    binary tree

---
+++ src/BinaryNode.java
@@ -0,0 +1,28 @@
+public class BinaryNode<T> {
+    public T key;
+    public BinaryNode rightChild;
+    public BinaryNode leftChild;
+    public BinaryNode(T Key){
+        this.key=Key;
+    }
+
+    public BinaryNode getRightChild() {
+        return rightChild;
+    }
+
+    public BinaryNode getLeftChild() {
+        return leftChild;
+    }
+
+    public T getKey() {
+        return key;
+    }
+
+    public void setLeftChild(BinaryNode leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public void setRightChild(BinaryNode rightChild) {
+        this.rightChild = rightChild;
+    }
+}

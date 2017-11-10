0c99f3854c7ac8fe6deddb24099e45a78a6c513b
karolyi15 <karolyi15@hotmail.com>
2017-11-10 12:53:19 -0600

    bubble sort

---
+++ src/BinaryTree.java
@@ -0,0 +1,21 @@
+public class BinaryTree<T> {
+
+    public BinaryNode<T> root;
+    public BinaryTree(){
+    }
+    public BinaryTree(T key){
+        this.root= new BinaryNode<>(key);
+    }
+    public void add(T key){
+        BinaryNode <T> New=new BinaryNode<>(key);
+        if (this.root==null){
+            this.root=new BinaryNode<>(key);
+        }else if(){
+
+        }
+
+    }
+    public void del(){
+
+    }
+}

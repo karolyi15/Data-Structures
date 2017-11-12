0fd72a62e2db6c6195d2c577faace63bb7a06ad6
karolyi15 <karolyi15@hotmail.com>
2017-11-11 20:11:29 -0600

    fix tree

--- src/org/tec/datastructures/BinaryNode.java
+++ src/org/tec/datastructures/BinaryNode.java
@@ -1,31 +1,47 @@
 package org.tec.datastructures;
 public class BinaryNode<T> {
 
-    public T key;
-    public BinaryNode rightChild;
-    public BinaryNode leftChild;
+    private int valor;
 
-    public BinaryNode(T Key){
-        this.key=Key;
+    private BinaryNode padre;
+    private BinaryNode hojaIzquierda;
+    private BinaryNode hojaDerecha;
+
+    /* Constructor */
+    public BinaryNode(int valor) {
+        this.valor = valor;
     }
 
-    public BinaryNode getRightChild() {
-        return rightChild;
+    /* Setters y Getters */
+    public void setValor(int valor) {
+        this.valor = valor;
     }
 
-    public BinaryNode getLeftChild() {
-        return leftChild;
+    public int getValor() {
+        return valor;
     }
 
-    public T getKey() {
-        return key;
+    public BinaryNode getPadre() {
+        return padre;
     }
 
-    public void setLeftChild(BinaryNode leftChild) {
-        this.leftChild = leftChild;
+    public void setPadre(BinaryNode padre) {
+        this.padre = padre;
     }
 
-    public void setRightChild(BinaryNode rightChild) {
-        this.rightChild = rightChild;
+    public BinaryNode getHojaIzquierda() {
+        return hojaIzquierda;
+    }
+
+    public void setHojaIzquierda(BinaryNode hojaIzquierda) {
+        this.hojaIzquierda = hojaIzquierda;
+    }
+
+    public BinaryNode getHojaDerecha() {
+        return hojaDerecha;
+    }
+
+    public void setHojaDerecha(BinaryNode hojaDerecha) {
+        this.hojaDerecha = hojaDerecha;
     }
 }
--- src/org/tec/datastructures/BinaryTree.java
+++ src/org/tec/datastructures/BinaryTree.java
@@ -1,18 +1,53 @@
 package org.tec.datastructures;
 public class BinaryTree<T> {
 
-    public BinaryNode<T> root;
+    private BinaryNode raiz;
 
-    public BinaryTree(T key){
-        this.root= new BinaryNode<>(key);
+    /* Contructores */
+    public BinaryTree( int valor ) {
+        this.raiz = new BinaryNode( valor );
     }
 
-    public void add(T key){
-        BinaryNode <T> New=new BinaryNode<>(key);
-        if (this.root==null){
-            this.root=new BinaryNode<>(key);    }
+    public BinaryTree( BinaryNode raiz ) {
+        this.raiz = raiz;
+    }
 
+    /* Setters y Getters */
+    public BinaryNode getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(BinaryNode raiz) {
+        this.raiz = raiz;
+    }
+
+    private void addNodo(BinaryNode nodo, BinaryNode raiz ) {
+
+        if ( raiz == null ) {
+
+            this.setRaiz(nodo);
         }
+        else {
 
+            if ( nodo.getValor() <= raiz.getValor() ) {
+
+                if (raiz.getHojaIzquierda() == null) {
+                    raiz.setHojaIzquierda(nodo);
+                }
+                else {
+                    addNodo( nodo , raiz.getHojaIzquierda() );
+                }
+            }
+            else {
+
+                if (raiz.getHojaDerecha() == null) {
+                    raiz.setHojaDerecha(nodo);
+                }
+                else {
+                    addNodo( nodo, raiz.getHojaDerecha() );
+                }
+            }
+        }
+    }
 
 }
